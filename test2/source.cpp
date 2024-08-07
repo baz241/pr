39f8d62f2e2228b49dc0c7116928d6f5244a6fd4
baz241 <manulmanulov425@gmail.com>
2024-08-07 09:41:45 +0300

    qwe

--- test2/source.cpp
+++ test2/source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <map>
 #include <set>
+#include <vector>
 
 using namespace std;
 
@@ -8,34 +9,63 @@ template <typename Element>
 ostream& operator<<(ostream& out, const vector<Element>& container);
 template <typename Element>
 ostream& operator<<(ostream& out, const set<Element>& container);
+template <typename Key, typename Value>
+ostream& operator<<(ostream& out, const map<Key, Value>& container);
+
+
+template <typename First, typename Second>
+ostream& operator<<(ostream& out, const pair<First, Second>& p) {
+    return out << '(' << p.first << ", "s << p.second << ')';
+}
 
 template <typename Container>
 void Print(ostream& out, const Container& container) {
     bool is_first = true;
     for (const auto& element : container) {
-        if (is_first) {
-            out << element;
-            is_first = false;
-            continue;
+        if (!is_first) {
+            out << ", "s;
         }
-        out << ", "s << element;
+        is_first = false;
+        out << element;
     }
 }
 
-template <typename Type>
-ostream& operator<<(ostream& out, const vector<Type>& container) {
+template <typename Element>
+ostream& operator<<(ostream& out, const vector<Element>& container) {
+    out << '[';
     Print(out, container);
+    out << ']';
     return out;
 }
 
-template <typename Type>
-ostream& operator<<(ostream& out, const set<Type>& container) {
+template <typename Element>
+ostream& operator<<(ostream& out, const set<Element>& container) {
+    out << '{';
     Print(out, container);
+    out << '}';
+    return out;
+}
+
+template <typename Key, typename Value>
+ostream& operator<<(ostream& out, const map<Key, Value>& container) {
+    out << '<';
+    Print(out, container);
+    out << '>';
     return out;
 }
 
 int main() {
     const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
-    cout << cats << endl; 
-    return 0;
+    cout << cats << endl;
+
+    const vector<int> ages = {10, 5, 2, 12};
+    cout << ages << endl;
+
+    const map<string, int> cat_ages = {
+        {"Мурка"s, 10},
+        {"Белка"s, 5},
+        {"Георгий"s, 2},
+        {"Рюрик"s, 12}
+    };
+    cout << cat_ages << endl;
 }
